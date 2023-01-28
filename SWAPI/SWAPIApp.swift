4c81538ef64468829650eb0e85501d543c6e4fb8
Travis Brigman <travis.brigman@hcahealthcare.com>
2023-01-28 09:10:59 -0600

    Initial Commit

---
+++ SWAPI/SWAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SWAPIApp.swift
+//  SWAPI
+//
+//  Created by Travis Brigman on 1/28/23.
+//
+
+import SwiftUI
+
+@main
+struct SWAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
